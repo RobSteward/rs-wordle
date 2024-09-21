8c9c9314cda4ff4b4599286e643f79e8f84cafeb
robsteward <robinscharf@gmail.com>
2024-09-21 16:06:57 +0200

    Final readme changes and firebase function attempt - not working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,37 +1,69 @@
-import { onRequest } from 'firebase-functions/v2/https'
-import * as logger from 'firebase-functions/logger'
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
 import {
   onDocumentCreated,
   onDocumentUpdated,
-} from 'firebase-functions/v2/firestore'
-import * as admin from 'firebase-admin'
+} from "firebase-functions/v2/firestore";
+import * as admin from "firebase-admin";
 
 export const helloWorld = onRequest((request, response) => {
-  logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
-})
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
 
-admin.initializeApp()
+admin.initializeApp();
 
-export const processWord = onDocumentCreated('scores/{documentId}', async (event) => {
-  const document = event.data
-  if (!document) {
-    logger.error('No document found in event data')
-    return
+export const processWord = onDocumentCreated(
+  "scores/{documentId}",
+  async (event) => {
+    const document = event.data;
+    if (!document) {
+      logger.error("No document found in event data");
+      return;
+    }
+
+    const word = document.get("LAST_GAME_WORD");
+    if (!word) {
+      logger.error("No word found in document");
+      return;
+    }
+
+    logger.info(`Word is: ${word}`);
+    const response = await defineWord(word);
+    logger.info(response);
+    return response;
   }
+);
 
-  const word = document.get('LAST_GAME_WORD')
-  if (!word) {
-    logger.error('No word found in document')
-    return
+export const processWordUpdate = onDocumentUpdated(
+  "scores/{documentId}",
+  async (event) => {
+    const document = event.data;
+    if (!document) {
+      logger.error("No document found in event data");
+      return;
+    }
+
+    const word = document.after.get("LAST_GAME_WORD");
+    if (!word) {
+      logger.error("No word found in document");
+      return;
+    }
+
+    logger.info(`Word is: ${word}`);
+    const response = await defineWord(word);
+    logger.info(response);
+    return response;
   }
+);
 
-  logger.info(`Word is: ${word}`)
-  const response = await defineWord(word)
-  logger.info(response)
-  return response
-})
-
-async function defineWord(word: string): Promise<any> {
-  return { LAST_GAME_WORD_DEFINITION: `Definition of ${word}` }
+/**
+ * Defines the word.
+ * @param {string} word - The word to define.
+ * @return {Promise<{LAST_GAME_WORD_DEFINITION: string}>} Word definition.
+ */
+async function defineWord(
+  word: string
+): Promise<{ LAST_GAME_WORD_DEFINITION: string }> {
+  return {LAST_GAME_WORD_DEFINITION: `Definition of ${word}`};
 }
